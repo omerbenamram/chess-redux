280274d7c865b3cc80f73bae6145ccd87745b42f
Ido <idoba12@gmail.com>
2017-08-26 15:11:42 +0300

    updated css

--- src/components/ChessBoardCell.js
+++ src/components/ChessBoardCell.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './ChessBoard.css';
 
-class ChessBoardCell extends React.Component {
 
-    render = () => {
-        const {onCellClick, placement, color, type, selectPiece} = this.props;
-
-        return (<div className='chessboard-cell' id={selectPiece} onClick={() => onCellClick(placement, {color, type})}>
-            {placement}
-            <div className={'piece '+ color + type}  />
-        </div>)
-    }
-}
-
-
-
+const ChessBoardCell = ({onCellClick, placement, color, type, selectPiece}) => (
+    <div className='chessboard-cell' id={selectPiece} onClick={() => onCellClick(placement, {color, type})}>
+        <div className={color + type}/>
+    </div>
+);
 
 export default ChessBoardCell;
